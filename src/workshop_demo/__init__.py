d97af4a26b476f79e5fcc6f661dce3edb13ffcc3
Draga Doncila Pop <ddoncila@gmail.com>
2021-12-15 14:50:38 +1100

    bring back imports precommit removed

--- src/workshop_demo/__init__.py
+++ src/workshop_demo/__init__.py
@@ -2,3 +2,15 @@ try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
+
+from ._dock_widget import SegmentationDiffHighlight, Threshold, segment_by_threshold
+from ._reader import napari_get_reader
+from ._writer import labels_to_zip
+
+__all__ = [
+    "napari_get_reader",
+    "labels_to_zip",
+    "segment_by_threshold",
+    "SegmentationDiffHighlight",
+    "Threshold",
+]
