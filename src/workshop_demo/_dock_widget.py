fbceb7a07d0a81a6419370761f77e90a9c4d205f
Draga Doncila Pop <ddoncila@gmail.com>
2021-12-15 12:05:16 +1100

    oops

--- src/workshop_demo/_dock_widget.py
+++ src/workshop_demo/_dock_widget.py
@@ -46,7 +46,7 @@ def segment_by_threshold(img_layer: "napari.layers.Image", threshold: Threshold)
     """
     with progress(total=0):
         # need to use threshold.value to get the function from the enum member
-        threshold_val = threshold.value(img_layer.data)
+        threshold_val = threshold.value(img_layer.data.compute())
         binarised_im = img_layer.data > threshold_val
         seg_labels = da.from_array(label(binarised_im))
 
